20a0332117256cd79c6a319678d403661c51939e
Brian <brianfsosa@gmail.com>
2022-04-23 17:23:20 -0300

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <h1 className='bg-red-900'>CRM</h1>
+  )
+}
+
+export default App
