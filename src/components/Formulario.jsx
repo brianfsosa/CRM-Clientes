e5c7fdc2a27cdec933e03fe1d0b85e105c0e7a7a
Brian <brianfsosa@gmail.com>
2022-04-26 17:34:34 -0300

    agregando validacines

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 function Formulario() {
+     
+  const nuevoClienteSchema = Yup.object().shape({
+    nombre:'',
+    empresa:'',
+    email:'',
+    telefono:'',
+    notas:''
+  })
+
+  const handleSubmit = (valores) => {
+        
+  }  
+
   return (
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto'>
         <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Agregar Cliente</h1>
-        <Formik>
+        <Formik
+            initialValues={{
+                nombre:'',
+                empresa:'',
+                email:'',
+                telefono:'',
+                notas:''
+            }}
+            onSubmit={ (values) => {
+                handleSubmit(values)
+            }}
+            validationSchema={nuevoClienteSchema}
+        >
+            {({errors}) => {
+                return(
             <Form
                 className='mt-10'
             >
@@ -19,7 +47,9 @@ function Formulario() {
                         type='text'
                         className='mt-2 block w-full p-3 bg-gray-50'
                         placeholder='Nombre del Cliente'
+                        name='nombre'
                     />
+                    <ErrorMessage name='nombre'/>
                 </div>
                 <div className='mb-4'>
                     <label 
@@ -31,6 +61,7 @@ function Formulario() {
                         type='text'
                         className='mt-2 block w-full p-3 bg-gray-50'
                         placeholder='Empresa del Cliente'
+                        name='empresa'
                     />
                 </div>
                 <div className='mb-4'>
@@ -43,6 +74,7 @@ function Formulario() {
                         type='email'
                         className='mt-2 block w-full p-3 bg-gray-50'
                         placeholder='Email del Cliente'
+                        name='email'
                     />
                 </div>
                 <div className='mb-4'>
@@ -55,6 +87,7 @@ function Formulario() {
                         type='tel'
                         className='mt-2 block w-full p-3 bg-gray-50'
                         placeholder='Teléfono del Cliente'
+                        name='telefono'
                     />
                 </div>
                 <div className='mb-4'>
@@ -68,6 +101,7 @@ function Formulario() {
                         type='text'
                         className='mt-2 block w-full p-3 bg-gray-50 h-40'
                         placeholder='Notas del Cliente'
+                        name='notas'
                     />
                 </div>
                 <input 
@@ -76,6 +110,7 @@ function Formulario() {
                     className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg hover:cursor-pointer hover:bg-blue-600'
                 />
             </Form>
+            )}}
         </Formik>
     </div>
   )
